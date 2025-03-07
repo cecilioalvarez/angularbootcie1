948db7d0e938fcd74adaec3a0ec543c6ecf90b76
cecilioAlvarez <cmac@arquitecturajava.com>
2025-03-07 09:50:18 +0100

    nuevos ok observable

--- src/app/formulario-libro/formulario-libro.component.ts
+++ src/app/formulario-libro/formulario-libro.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Libro } from '../libro';
 import { FormsModule } from '@angular/forms';
+import { LibroRestService } from '../libro-rest.service';
 
 @Component({
   selector: 'app-formulario-libro',
@@ -11,10 +12,18 @@ import { FormsModule } from '@angular/forms';
 export class FormularioLibroComponent {
 
   libro:Libro= {} as Libro;
+  constructor(private libroRestService:LibroRestService) {
+
+  
+  }
 
   public insertar() {
 
-    console.log(this.libro);
+    //console.log(this.libro);
+
+    this.libroRestService.insertar(this.libro).subscribe((datos)=>{
+      console.log("insertado");
+    })
 
   }
 
